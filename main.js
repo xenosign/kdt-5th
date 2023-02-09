a20332544e049c2bf73cc8011eef979c4c74219e
kdtTetz <xenosign@naver.com>
2023-02-10 02:45:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j <= i; j++) {
-    console.log(`*`);
-  }
-  console.log("\n");
+// continue
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) continue;
+  console.log(`${i} 번 입니다!`);
 }
